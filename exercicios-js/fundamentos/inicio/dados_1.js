e52e1b7fdda760a8471d4db0a2629de6279cf7a1
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 18:59:16 -0300

    dados1

---
+++ exercicios-js/fundamentos/inicio/dados_1.js
@@ -0,0 +1,16 @@
+//DECLARANDO VARIÁVEIS no programa. (let é a forma moderna)
+var nome = "Caneta"; 
+var quantidade = 10; 
+var preco = 6.4;
+let imposto = 1.5;
+let precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+// Mudando o valor da variável
+nome = "Caneta BIC";
+console.log(nome);
