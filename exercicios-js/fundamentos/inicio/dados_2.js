aef0bc3be4119d15ad915b4ad52bdf3eb7eeeb80
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 19:16:11 -0300

    dados

--- exercicios-js/fundamentos/inicio/dados_2.js
+++ exercicios-js/fundamentos/inicio/dados_2.js
@@ -2,9 +2,10 @@ let preco = 19.90;
 let desconto = 0.4;
 let precoComDesconto = preco * (1 - desconto);
 
-console.log(precoComDesconto);
+console.log("Preço com Desconto: " + precoComDesconto);
 //console.log(19.9 * 0.6);
 //console.log(preco * (1 - desconto));
+
 
 let nome = "Caderno"; // String (texto) -> sequencia de símbolos
 let categoria = "Papelaria";
