eee546c23970caa91d85d668e1019c4fe1aa030e
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 18:43:14 -0300

    desafiosimples

---
+++ exercicios-js/fundamentos/inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,23 @@
+/* DESAFIO INICIAL
+Exibir os númentos ímpares no Bloco 1
+1, 3, 5, 7, 9
+
+Exibir os númentos ímpares no Bloco 2
+2, 4, 6, 8, 10
+*/
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
+// Resposta do Professor foi igual
+
