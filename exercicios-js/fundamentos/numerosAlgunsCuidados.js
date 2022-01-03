5e5bdc0b04d9f5109e13809298c9a31fe4189188
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 09:28:25 -0300

    Cuidados

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -5,9 +5,3 @@ console.log("Show!" * 2) // Em Python a string é multiplicada em JS não!
 console.log(0.1 + 0.7) // por conta da especificação(IEEE) da operação de tipo flutuante - Não é preciso!!!
 // console.log(10.toString())
 console.log((10.345).toFixed(2))
-
-
-
-
-
-
