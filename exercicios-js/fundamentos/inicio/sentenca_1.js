f90371d540933c86d4b4a4f4d22774d6ce1af1fa
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 17:35:04 -0300

    sentenca

---
+++ exercicios-js/fundamentos/inicio/sentenca_1.js
@@ -0,0 +1,10 @@
+console.log("Bom dia!") // Melhor forma, na minha opinião!
+console.log("Boa tarde!") 
+console.log("Boa noite!") 
+
+//Não fica tão organizado! e pode usar o ponto e vírgula para finalizar a sentença do código
+console
+    .log(
+        "Legal!!!"
+    );
+
