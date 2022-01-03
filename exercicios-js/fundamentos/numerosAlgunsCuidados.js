9eb87e2460f5222acf2a7ca470fda45fa3929f67
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 10:07:03 -0300

    aula1

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,6 +1,10 @@
 console.log(7/ 0) // Retorna Infinity 
 console.log(7/ 0.000001) // Retorna valor grande
 console.log("10" / 2) // Em outras linguagens não funcionaria!
+
+console.log("3" + 2) // vai concatenar porque a string ganha
+console.log("3" - 2) // vai subtrair 
+
 console.log("Show!" * 2) // Em Python a string é multiplicada em JS não!
 console.log(0.1 + 0.7) // por conta da especificação(IEEE) da operação de tipo flutuante - Não é preciso!!!
 // console.log(10.toString())
--- exercicios-js/fundamentos/strings.js
+++ exercicios-js/fundamentos/strings.js
@@ -8,3 +8,13 @@ console.log(escola.indexOf('3')) // Saber onde ele está na string
 console.log(escola.substring(1)) // não inclui o primeiro caracter
 console.log(escola.substring(0, 3)) // do indice 0 ao 3 mas não incluiu o 3
 
+//concatenando...
+console.log('Escola '.concat(escola).concat("!")) // melhor forma
+console.log('Escola '+ escola + "!") 
+
+//Substituindo... Replace
+console.log(escola.replace(3, 'e'))
+console.log(escola.replace(/\d/, 'e'))
+console.log(escola.replace(/\w/g, 'e')) // g é de global sem ele muda apenas a primeira letra
+
+console.log('Ana,Maria,Pedro'.split(','))
