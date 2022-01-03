239a55de3f39a793edbb5ec357229afe1967aa14
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 20:08:49 -0300

    dados3

---
+++ exercicios-js/fundamentos/inicio/dados_3.js
@@ -0,0 +1,19 @@
+// NUMBER
+// Numeros inteiros ou com casas decimais (ponto flutuante) são NUMBERs
+let idade = 44;
+
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+// BOOLEAN
+// Verdadeiro e falso
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+// STRING
+// textos 
+console.log(typeof "Teste aspas duplas");
+console.log(typeof 'Teste aspas duplas');
---
+++ exercicios-js/fundamentos/inicio/dados_4.js
@@ -0,0 +1,7 @@
+//let a = 3;
+const a = 3;
+// ...
+// a = a + 10; // só muda se for var ou let 
+// ....
+
+console.log(a);
