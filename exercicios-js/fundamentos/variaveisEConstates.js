26cc6bb9b4ed666e85458d162d2b249c6302b23d
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 07:55:52 -0300

    aula1

---
+++ exercicios-js/fundamentos/variaveisEConstates.js
@@ -0,0 +1,28 @@
+var a = 3 // Identificadores "a", procure um bom nome!
+let b = 4 // Também cria uma variável e o let é a forma moderna de declarar uma variável. PREFIRA LET
+
+var a = 30 
+b = 40
+//let b = 40
+/*
+let b = 40
+    ^
+SyntaxError: Identifier 'b' has already been declared
+*/
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+/*c = 50
+ c = 50 
+  ^
+
+TypeError: Assignment to constant variable.
+*/
+
+console.log (c)
+
