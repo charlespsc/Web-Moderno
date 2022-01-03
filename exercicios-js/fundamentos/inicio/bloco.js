57b8ba33fff881efe720f6cf07fa348bd044a51f
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 18:33:04 -0300

    bloco

---
+++ exercicios-js/fundamentos/inicio/bloco.js
@@ -0,0 +1,10 @@
+// Par de chaves para definir um bloco, somente bloco dentro de bloco.
+{
+    console.log("Passo #01")
+    {
+        console.log("Passo #02")
+        {
+            console.log("Passo #03")
+        } 
+    }
+}
