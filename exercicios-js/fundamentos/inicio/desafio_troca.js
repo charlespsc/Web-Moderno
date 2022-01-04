440f17464ee3e9d85e9c12123f0eed8eaa39ddda
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 21:54:07 -0300

    troca

---
+++ exercicios-js/fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,27 @@
+/*Desafio
+
+let a = 7
+let b = 94
+
+console.log(a)
+console.log(b)
+
+// Trocar o valor das variáveis... a = 94 e b = 7
+a = b
+b = a - 87
+
+console.log(a)
+console.log(b)
+*/
+
+// Resposta do Professor
+let a = 7
+let b = 94
+
+let temp = a
+
+a = b
+b = temp
+
+console.log("Valor de A = " + a)
+console.log("Valor de A = " + b)
