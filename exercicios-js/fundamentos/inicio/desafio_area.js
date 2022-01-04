2f01e408540a8916e455df524d56f893cee687f8
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-03 21:36:38 -0300

    area

---
+++ exercicios-js/fundamentos/inicio/desafio_area.js
@@ -0,0 +1,12 @@
+// PI * raio * raio
+// PI = 3.141592
+
+const PI = 3.1415;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + "m2.");
+
+areaCirc = Math.PI * raio * raio; // O PI é mais preciso!
+console.log("O valor da área é " + areaCirc + "m2.");
+
