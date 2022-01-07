0d80da176b6030b15f08433e9663bf5560fc16b2
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-06 23:46:12 -0300

    var1

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{ 
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+console.log(sera) // Mesmo fora do bloco será visível
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+// console.log(local) // Não será visível
