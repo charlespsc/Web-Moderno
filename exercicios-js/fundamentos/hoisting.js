ca13f1cd4e5d1991b87f269f87e1d98b494a2514
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-07 19:28:14 -0300

    hoisting

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,19 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+// com função
+function teste(){
+    console.log('a =', a)
+    var a = 2
+    console.log('a =', a)
+}
+
+teste()
+console.log('a =', a)
+
+// quando usado com LET não tem hoisting
+co
+let b = 2
+
+
