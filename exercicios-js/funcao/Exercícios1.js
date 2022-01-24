7cb5c25277078cec76da2e8c35bf1cb0bdee45b0
Charles Pereira <charlespereira.fln@gmail.com>
2022-01-23 22:29:19 -0300

    exe

--- exercicios-js/funcao/Exercícios1.js
+++ exercicios-js/funcao/Exercícios1.js
@@ -1,20 +1,16 @@
-/*
-01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
-multiplicação e divisão desses valores. 
-*/
+/* 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.  */
 function calcularOperacoes (operador1, operador2) {
     console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
 }
 
 calcularOperacoes(1, 2)
 
-/*
-02) Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+/* 02) Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
 Equilátero: Os três lados são iguais. Isósceles: Dois lados iguais. Escaleno: Todos os lados são diferentes.
 Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua classificação quanto
 ao tamanho de seus lados. (Neste exemplo deve-se abstrair as condições matemáticas de existência de um
-triângulo).
-*/
+triângulo). */
 function classificaTriangulo (lado1, lado2, lado3) {
     if(lado1 == lado2 && lado2 == lado3){
         return 'Equilátero'
@@ -29,9 +25,7 @@ console.log(classificaTriangulo(2, 2, 2));
 console.log(classificaTriangulo(2, 3, 3));
 console.log(classificaTriangulo(2, 3, 4));
 
-/*
-03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
-*/
+/* 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente. */
 function expoente (base, expoente) {
     //Podemos fazer de 2 modos
     //Método antigo:
