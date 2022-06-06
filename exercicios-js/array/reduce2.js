64cf9a858bbd45ab877f52400665be9ac9c9856f
Charles Pereira <charlespereira.fln@gmail.com>
2022-06-05 22:50:23 -0300

    reduce2

--- exercicios-js/array/reduce2.js
+++ exercicios-js/array/reduce2.js
@@ -1,8 +1,8 @@
 const alunos = [
-    { nome: 'João', nota: 7.3, bolsista: false },
-    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'João', nota: 7.3, bolsista: true },
+    { nome: 'Maria', nota: 9.2, bolsista: false },
     { nome: 'Pedro', nota: 9.8, bolsista: false },
-    { nome: 'Ana', nota: 8.7, bolsista: true },
+    { nome: 'Ana', nota: 8.7, bolsista: false },
 ]
 
 // Desafio 1: Todos os alunos são bolsista?
@@ -12,3 +12,4 @@ console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
 // Desafo 2: Algum aluno é bolsista?
 const algumBolsista = (resultado, bolsista) => resultado || bolsista
 console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
+ 
