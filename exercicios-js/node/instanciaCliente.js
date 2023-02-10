aac593d9bf9989e254dbb850b6e9e9980bfa5ef0
Charles Pereira <charlespereira.fln@gmail.com>
2023-02-10 00:46:21 -0300

    instancias

--- exercicios-js/node/instanciaCliente.js
+++ exercicios-js/node/instanciaCliente.js
@@ -6,4 +6,8 @@ const contadorD = require('./instanciaNova')
 
 contadorA.inc()
 contadorA.inc()
-console.log(contadorB.valor)
+console.log(contadorA.valor, contadorB.valor)
+
+contadorC.inc()
+contadorC.inc()
+console.log(contadorC.valor, contadorD.valor)
