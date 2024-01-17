3f99e990542bdefbfb82aa2f4569a93d3b0913b1
Charles Pereira <charlespereira.fln@gmail.com>
2024-01-17 19:54:10 -0300

    TABELA VERDADE

---
+++ exercicios-js/fundamentos/inicio/tabelaVerdade.js
@@ -0,0 +1,23 @@
+/* TABELA VERDADE
+
+V e V = V
+V e F = F
+F e V = F
+F e F = F
+
+V ou V = V
+V ou F = V
+F ou V = V
+F ou F = F
+
+-> OU Exclusivo verdadeiro
+V xor V = F
+V xor F = V
+F xor V = V
+F xor F = F
+
+-> Negação lógica
+!V = F
+!F = V
+
+*/
